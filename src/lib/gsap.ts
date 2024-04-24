cc41e2fbb97ac068f168fff3449057aa34cd38d2
C-Mierez <carlos.mierez20@gmail.com>
2024-04-24 18:36:17 -0300

    🧑‍💻 Global gsap config

---
+++ src/lib/gsap.ts
@@ -0,0 +1,12 @@
+import gsap from "gsap";
+import useGSAP from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
+
+gsap.defaults({
+    duration: 1,
+    ease: "power2.inOut",
+});
+
+export * from "gsap";
+export * from "@gsap/react";
