f91ada3361ac70fe6380293abe7043cdb49500b9
C-Mierez <carlos.mierez20@gmail.com>
2024-04-25 19:48:58 -0300

    💫 Pulse Grid animation

--- src/app/(homepage)/_components/pulse-grid.tsx
+++ src/app/(homepage)/_components/pulse-grid.tsx
@@ -1,14 +1,109 @@
+"use client";
+
+import { useRef } from "react";
+import { gsap, useGSAP } from "~/lib/gsap";
+import usePrefersReducedMotion from "~/hooks/use-prefers-reduced-motion";
+
 export default function PulseGrid() {
+    const svgRef = useRef(null);
+    const prefersReducedMotion = usePrefersReducedMotion();
     const grid = [14, 30] as const;
+
+    useGSAP(
+        () => {
+            if (prefersReducedMotion) {
+                gsap.set(".pulse-grid-item", { opacity: 0.3, scale: 1 });
+                gsap.set(svgRef.current, { opacity: 1 });
+                return;
+            }
+
+            gsap.set(".pulse-grid-item", {
+                opacity: 0,
+                transformOrigin: "center",
+                color: "#fff",
+            });
+            gsap.set(svgRef.current, { opacity: 1 });
+
+            const t1 = gsap.timeline();
+
+            // Enter animation
+            t1.to(".pulse-grid-item", {
+                keyframes: [
+                    { opacity: 0, duration: 0 },
+                    {
+                        opacity: 0.25,
+                        rotate: "+=180",
+                        color: "#FFC400",
+                        scale: 2.4,
+                        duration: 0.6,
+                        stagger: {
+                            amount: 1.8,
+                            grid: grid,
+                            from: "center",
+                        },
+                    },
+                    {
+                        opacity: 0.3,
+                        rotate: "+=180",
+                        scale: 1,
+                        color: "#fff",
+                        delay: -1.8,
+                        duration: 0.6,
+                        stagger: {
+                            amount: 2,
+                            grid: grid,
+                            from: "center",
+                        },
+                    },
+                ],
+            });
+
+            // Loop animation
+            t1.to(".pulse-grid-item", {
+                keyframes: [
+                    {
+                        opacity: 0.3,
+                        rotate: "+=180",
+                        color: "#FFC400",
+                        scale: 2.4,
+                        duration: 0.6,
+                        stagger: {
+                            amount: 1.8,
+                            grid: grid,
+                            from: "center",
+                        },
+                    },
+                    {
+                        opacity: 0.3,
+                        rotate: "+=180",
+                        scale: 1,
+                        color: "#fff",
+                        delay: -1.8,
+                        duration: 0.6,
+                        stagger: {
+                            amount: 2,
+                            grid: grid,
+                            from: "center",
+                        },
+                    },
+                ],
+                repeat: -1,
+                repeatDelay: 8,
+                delay: 8,
+            });
+        },
+        { scope: svgRef },
+    );
 
     return (
         <svg
+            ref={svgRef}
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 935 425"
             className="absolute -z-10"
             id="pulse-grid"
-            opacity={1}
+            opacity={0}
             style={{
                 maskImage:
                     "linear-gradient(transparent 1%, black,  black, transparent)",
