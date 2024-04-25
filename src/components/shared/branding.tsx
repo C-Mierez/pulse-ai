c66e4a02680f56c3817a04aa907f3626670a9eee
C-Mierez <carlos.mierez20@gmail.com>
2024-04-24 22:16:50 -0300

    💄 Removed logo from branding

--- src/components/shared/branding.tsx
+++ src/components/shared/branding.tsx
@@ -3,9 +3,6 @@ import SiteLogo from "../svg/site-logo";
 export default function Branding() {
     return (
         <>
-            <div className="h-6 w-7">
-                <SiteLogo color="#fff" />
-            </div>
             <h1 className="font-sans text-2xl font-medium leading-[1.5rem]">
                 Pulse.<span className="text-accent-light">AI</span>
             </h1>
