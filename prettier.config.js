fc27d689b758260c02422b986baa78fcb9b7adbc
C-Mierez <carlos.mierez20@gmail.com>
2024-04-27 20:36:44 -0300

    🧑‍💻 Tailwind class sorting inside functions

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,8 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
-  tabWidth: 4,
+    plugins: ["prettier-plugin-tailwindcss"],
+    tabWidth: 4,
+    tailwindFunctions: ["clsx", "cva"],
 };
 
 export default config;
